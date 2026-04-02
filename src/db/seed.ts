856e44b132b3806a29922e94f2f59fb44489aa36
James Kirk <jameskirk@gmail.com>
2026-04-02 19:24:52 +1100

    multiple user chats

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -10,6 +10,7 @@ const SALT_ROUNDS = 10;
 const users = [
   { username: 'user1', password: 'password1' },
   { username: 'user2', password: 'password2' },
+  { username: 'user3', password: 'password3' },
 ];
 
 async function seed() {
