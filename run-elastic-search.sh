ee1a4881de8ed6da1c2a2eb5710e3b4c9fb5fb98
James Kirk <jameskirk@gmail.com>
2026-04-10 14:06:40 +1000

    file cleanup

--- run-elastic-search.sh
+++
@@ -1,5 +0,0 @@
-docker run -d --name elasticsearch \
-    -p 9200:9200 \
-    -e "discovery.type=single-node" \
-    -e "xpack.security.enabled=false" \
-    docker.elastic.co/elasticsearch/elasticsearch:9.0.0
